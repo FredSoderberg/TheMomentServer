accf9a4e6c0abe8d0f48426de44360cd8b8cf830
DeeAwesomeOne <Desiree.Bjorkman.4477@student.uu.se>
2018-02-07 22:39:08 +0100

    Update test.php
    Php resv and send JSON

--- test.php
+++ test.php
@@ -1,3 +1,21 @@
 <?php
-echo "Fredrik e en hest";
+$jsonobj = $_GET["jsonobj"];
+$obj = json_decode($jsonobj);
+$name = $obj -> {"name"};
+$answer = $obj -> {"answer"};
+if ($answer == "dinmamma") {
+$price = 1;
+} else if ($answer == "dinpappa") {
+$price = 1;
+} else {
+$price = 0;
+}
+if ($price == 0) {
+$status = "not-accepted";
+} else {
+$status = "accepted";
+}
+$array = array("name" => $name, "answer" => $answer, "price" => $price, "status" => $status);
+echo json_encode($array);
+
 ?>
