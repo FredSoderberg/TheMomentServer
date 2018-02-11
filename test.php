f0e6d3022e06689e3d318f3b1f3ba27802c866d2
DeeAwesomeOne <Desiree.Bjorkman.4477@student.uu.se>
2018-02-11 14:17:59 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,23 +1,22 @@
 <?php
 //Decodes JSON
 $jsonobj = $_GET["jsonobj"];
-$obj = json_decode($jsonobj, true);
+$obj = json_decode($jsonobj,true);
+
+/*
 //$typeInSever = $obj["type"]);
-foreach ($obj as $key => $value) {
-	foreach ($value as $key => $value) {
-		$typeInSever = $value -> {"typeInSever"};
-
-	}
+foreach ($obj as $value){
+	foreach
+	$typeInSever = $obj["typeInSever"];
 }
-
+*/
 //$name = $obj[1] -> {"name"};
-//$typeInSever = $obj->type->typeInSever;
+$typeInServer = $obj[1] -> {"typeInServer"};
 
 //Just to see what type if has gotten out:
-$array = array("name" => $name, "answer" => null, "typeInSever" => $typeInSever);
-
+$array = array("name" => $name, "answer" => null, "typeInServer" => $typeInServer);
 //Cases, will be used to determine what to do when called. 
-switch ($typeInSever){
+switch ($typeInServer){
 	case "basic":	
 		$name2 = "Henri";
 		$answer2 = FALSE;
@@ -31,5 +30,6 @@ switch ($typeInSever){
 		echo json_encode($array);
 		break;
 }
+//echo $jsonobj
 echo json_encode($array); //Should return with null in the object(right now a player object)
 ?>
