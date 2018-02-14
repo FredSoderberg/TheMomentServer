2001e93caba9342c6a1e57d36f6afa57fb268f26
Fredrik Söderberg <soderbergh@gmail.com>
2018-02-14 19:56:20 +0100

    Result Page  - setup necessary functionality and network coms - added missing field to mock json

--- getFromDB.php
+++ getFromDB.php
@@ -25,19 +25,20 @@ function getRandomRoom() {
     //TODO add player to random room and confirm it doesnt exced max
     //echo '{"ID": 1, "numOfPlayers": 1, "playerList": [ {"Player": {"id": 1, "name": "Uffe", "score": 10, "answer": true, "claim": {"claim": "Vi har", "correctAnswer": true }}}]}';
     echo '{
-        "ID": 1,
+  "ID": 10,
   "numOfPlayers": 1,
   "playerList": [
     {
-        "Player": {
+      "Player": {
         "id": 1,
         "name": "Uffe",
         "score": 10,
         "answer": true,
         "claim": {
-            "claim": "Vi har",
+          "claim": "Vi har",
           "correctAnswer": true
-        }
+        },
+        "isPlayer": true
       }
     }
   ]
@@ -47,19 +48,20 @@ function getRandomRoom() {
 function getRoomByID($json) {
     //TODO query server with $json for room and encode it and send it back
     echo '{
-        "ID": 10,
+  "ID": 10,
   "numOfPlayers": 1,
   "playerList": [
     {
-        "Player": {
+      "Player": {
         "id": 1,
         "name": "Uffe",
         "score": 10,
         "answer": true,
         "claim": {
-            "claim": "Vi har",
+          "claim": "Vi har",
           "correctAnswer": true
-        }
+        },
+        "isPlayer": true
       }
     }
   ]
