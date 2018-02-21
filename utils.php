8c5fdecdbe36a5bde1f2ed01b45f04f710364081
Fredrik Söderberg <soderbergh@gmail.com>
2018-02-21 14:11:30 +0100

    started to implement isRoundDone

--- utils.php
+++ utils.php
@@ -1,5 +1,4 @@
 <?php
-
 $form_action_func = $_GET['function'];
 $json = $_GET['jsonobj'];
 if(isset($form_action_func))
@@ -14,9 +13,44 @@ if(isset($form_action_func))
     }
 }
 
+isRoundDone("[6,1]");
+
 function isRoundDone($json) {
+    $list = json_decode($json);
+    $roomID = current($list);
+    next($list);
+    $roundNo = current($list);
+
+    $connection = db_connect();
+    if ($query = mysqli_prepare($connection, "SELECT * FROM Player WHERE RoomID=?")) {
+        /* bind parameters for markers */
+        mysqli_stmt_bind_param($query, "i", $roomID);
+
+        /* execute query */
+        if(!mysqli_stmt_execute($query)) {
+            echo "Failed";
+        }
+        /* bind result variables */
+        mysqli_stmt_bind_result($stmt, $district);
+
+        /* fetch value */
+        mysqli_stmt_fetch($stmt);
+
+        printf("GOT - %s",$district);
+        /* close statement */
+        mysqli_stmt_close($query);
+    }
+    /* close connection */
+    mysqli_close($connection);
     //TODO loop over all players in in room id and check if all players are round in json or above. if so echo true otherwise false! :D
-    echo $json;
+
+
+/*
+    $string = $roomID;
+    $string .= "-";
+    $string .= $roundNo;
+    echo $string;
+*/
 }
 
 function isServerAndDBUp() {
